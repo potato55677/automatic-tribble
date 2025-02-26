28bedc5aadd571560db0db008cf1c0a4cf57f12e
John <studyfinalexam@gmail.com>
2025-02-27 01:18:30 +0530

    gitpod.sh

--- gitpod.sh
+++ gitpod.sh
@@ -4,7 +4,7 @@
 username="user"
 password="root"
 
-# Set default CRP value (User will enter it)
+# Set default CRP value
 CRP=""
 
 # Set default Pin value
@@ -22,17 +22,21 @@ echo "User created and configured with username '$username' and password '$passw
 
 echo "Installing necessary packages"
 sudo apt update
-sudo apt install -y ubuntu-desktop gnome-terminal dbus-x11 wget curl
+sudo apt install -y ubuntu-desktop tightvncserver wget
 
 echo "Setting up Chrome Remote Desktop"
+echo "Installing Chrome Remote Desktop"
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 
-echo "Configuring Chrome Remote Desktop for Ubuntu Desktop"
-echo "exec /usr/bin/gnome-session" | sudo tee /etc/chrome-remote-desktop-session
-
-sudo adduser "$username" chrome-remote-desktop
+echo "Installing Desktop Environment"
+export DEBIAN_FRONTEND=noninteractive
+sudo apt install --assume-yes ubuntu-desktop
+echo "exec /etc/X11/Xsession /usr/bin/gnome-session" | sudo tee /etc/chrome-remote-desktop-session
+sudo apt remove --assume-yes gnome-terminal
+sudo apt install --assume-yes xscreensaver
+sudo systemctl disable lightdm.service
 
 echo "Installing Google Chrome"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
@@ -42,7 +46,7 @@ sudo apt install --assume-yes --fix-broken
 # Prompt user for CRP value
 read -p "Enter CRP value: " CRP
 
-echo "Finalizing Setup"
+echo "Finalizing"
 if [ "$Autostart" = true ]; then
     mkdir -p "/home/$username/.config/autostart"
     link="https://youtu.be/d9ui27vVePY?si=TfVDVQOd0VHjUt_b"
@@ -52,15 +56,10 @@ if [ "$Autostart" = true ]; then
     sudo chown "$username:$username" "/home/$username/.config"
 fi
 
-# Restart CRD service properly
-sudo kill -9 $(pgrep -f chrome-remote-desktop) 2>/dev/null
-sudo rm -rf /tmp/.X11-unix/X*
-sudo /opt/google/chrome-remote-desktop/chrome-remote-desktop --stop
-sudo /opt/google/chrome-remote-desktop/chrome-remote-desktop --start
-
-# Start CRD session
+sudo adduser "$username" chrome-remote-desktop
 command="$CRP --pin=$Pin"
 sudo su - "$username" -c "$command"
+sudo service chrome-remote-desktop start
 
-echo "Setup Completed Successfully"
+echo "Finished Successfully"
 while true; do sleep 10; done
